e229160debe27e4d3f77b9abef1ac17dbafe3cf5
Shubhechchha Dahal <dahal.shubhechchha@gmail.com>
2022-03-31 21:24:51 +0200

    contact component added

--- src/components/About.js
+++ src/components/About.js
@@ -1,51 +1,23 @@
 import React from 'react';
-
+ 
 const About = () => {
   return (
-  <div> 
-    <div className='card'>
-      <div className='card-content'>
-        <h6 className='mt-bottom'>
+  <div>
+    <div className='card z-depth-0 hide-on-med-and-down pt '>
+      <div className='card-content center social'>
+        <h4 className='mt-bottom'>
         <strong>
           ABOUT ME
         </strong>
-        </h6>
-        <p className='grey-text'>I am a highly motivated and diligent innovation management graduate. 
-        My studies have given me a thorough understanding of digital business processes and IT Governance through practical use cases. 
+        </h4>
+        <p className='grey-text'>I am a highly motivated and diligent innovation management graduate.
+        My studies have given me a thorough understanding of digital business processes and IT Governance through practical use cases.
         I aspire to put my knowledge into practice in a fast-paced business environment.</p>
        </div>
-
-      <div className='card-action'>
-        <h6>
-         <strong> PERSONAL INFO</strong>
-        </h6>
-
-        <div className="row mt">
-            <div className="col xl6 l6 m6 s12">
-              <p>
-                <strong>Phone:</strong> +49 172 716 2305
-              </p>
-              <p>
-                <strong>Address:</strong> Balthasar Neumann Platz-1, Brühl 50321
-              </p>
-            </div>
-            <div className="col xl6 l6 m6 s12">
-              <h6>
-                <strong>Languages</strong> 
-                <p>Deutsch B1 Certified, English
-                   Nepali, Hindi
-                </p> 
-                
-                
-              </h6>
-            
-            </div>
-          </div>
-        </div> 
-
     </div>
   </div>
   )
 }
-
+ 
 export default About
+ 
---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+ 
+const Contact = () => {
+  return (
+<div>
+    <div className='card cyan darken-3 z-depth-0'>  
+    <div className='card-content'>
+        <h6 className="white-text">
+            <strong> CONTACT</strong>
+        </h6>
+    <hr />
+ 
+        <p className="black-text text-lighten-3 pt">
+        <i className="fas fa-map-marker-alt"></i> Balthasar Neumann Platz-1, Brühl 50321
+        </p>
+        <p className="black-tect text-lighten-3 pt">
+        <i className="fa-solid fa-phone"></i> +49 172 716 2305
+        <br/>
+        <a href="https://gmail.com" mailto="dahal.shubhechchha@gmail.com" target='blank'>
+        <i className="fa-solid fa-envelope"></i> dahal.shubhechchha@gmail.com
+        </a>
+        </p>
+ 
+ 
+    </div>
+    </div>
+</div>
+  )
+}
+ 
+export default Contact
